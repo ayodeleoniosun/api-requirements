9de0b33acc78e44e925ec5c457c379af522650d7
Ayodele Oniosun <heywire4u@gmail.com>
2022-09-19 14:22:24 +0100

    chore: setup models

---
+++ app/Domain/Product/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getPriceAttribute()
+    {
+        return $this->price / 100;
+    }
+
+    public function setPriceAttribute(int $price)
+    {
+        return $this->attributes['price'] = $price * 100;
+    }
+}
